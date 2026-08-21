6de28984644f93d777c05fff8c8519698fe13686
komal-0527 <komalkalsoom27@gmail.com>
2026-08-21 18:27:05 +0500

    add

--- script.js
+++
@@ -1,7 +0,0 @@
-function calculate() {
-    const income = document.getElementById("income").value;
-    const tax = calculateTax(income);
-
-    document.getElementById("result").innerText =
-        "Calculated Tax: $" + tax.toFixed(2);
-}
